91dd38fed1d4f2efd582eecdff2bea7cc7852df3
Kristofer Leifland <kristofer.leifland@gmail.com>
2013-09-14 13:20:37 +0200

    Refactoring IntTraits.

--- Rational/IntTraits.h
+++ Rational/IntTraits.h
@@ -5,38 +5,35 @@ using namespace std;
 
 namespace IntTraits {
 
+/* NextType trait, used to get a larger int type */
 template <typename intT>
-struct IntTraits {
+struct NextType {
   static bool HasNextType() { return false; }
 };
 
 template <>
-struct IntTraits<short> {
-  typedef int NextType;
+struct NextType<short> {
+  typedef int Type;
   static bool HasNextType() { return true; }
 };
 
 template <>
-struct IntTraits<int> {
-  typedef long NextType;
+struct NextType<int> {
+  typedef long Type;
   static bool HasNextType() { return true; }
 };
 
 template <>
-struct IntTraits<long> {
-  typedef long long NextType;
+struct NextType<long> {
+  typedef long long Type;
   static bool HasNextType() { return true; }
 };
 
+/* LargestType trait, used to get the larger of two types */
+// TODO: Handle unsigned types
 template <typename T, typename U>
 struct LargestType {
   typedef typename conditional<sizeof(U) <= sizeof(T), T, U>::type Type;
 };
-
-template <typename T, typename U>
-void TestFunc(T a, U b) {
-  cout << typeid(LargestType<T, U>::Type).name() << endl;
-}
-
 
 }
